6708ee05b135a6154806f5c388af6638c5622f95
Artyom Donskih <totzhe@yandex.ru>
2014-03-28 02:06:31 +0400

    Первая версия алгоритма Куттера

---
+++ KutterAlgorithm/KutterAlgorithm/KutterEncipherer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace KutterAlgorithm
+{
+    public class KutterEncipherer
+    {
+        private int _size = 0;
+        public Bitmap Encode(string text, Bitmap image, int delta, double alpha)
+        {
+            var binText = text.ToBitString();
+            int size = binText.Length;
+            _size = size;
+            binText = size.ToBitString() + binText;
+            int x = delta, y = delta;
+            foreach (var bit in binText)
+            {
+                var pixel = image.GetPixel(x, y);
+                var r = pixel.R;
+                var g = pixel.G;
+                var b = pixel.B;
+                var l = 0.3 * r + 0.59 * g + 0.11 * b;
+
+                var diff = (byte)(alpha * l);
+                if (bit == '1')
+                {
+                    b += diff;
+                    if (b > 255)
+                        b = 255;
+                }
+                else
+                {
+                    if (diff > b)
+                        b = 0;
+                    else
+                        b -= diff;
+                }
+
+                image.SetPixel(x, y, Color.FromArgb(pixel.A, r, g, b));
+
+                x += (delta + 1);
+                if (x >= image.Width - delta)
+                {
+                    y += (delta + 1);
+                    x = delta;
+                    if (y >= image.Height - delta)
+                        throw new Exception("The image is too small.");
+                }
+            };
+            return image;
+        }
+
+        public string Decode(Bitmap image, int delta, double alpha)
+        {
+            var result = "";
+            int x = delta, y = delta;
+            int bitCount = 0;
+            int length = -1;
+            while (result.Length < length || length < 0)
+            {
+                var pixel = image.GetPixel(x, y);
+                var b = pixel.B;
+                double bAvg = 0;
+                for (int i = x - delta; i <= x + delta; i++)
+                {
+                    if (i != x)
+                    {
+                        bAvg += image.GetPixel(i, y).B;
+                    }
+                }
+                for (int i = y - delta; i <= y + delta; i++)
+                {
+                    if (i != y)
+                    {
+                        bAvg += image.GetPixel(x, i).B;
+                    }
+                }
+                bAvg /= 4 * delta;
+
+                if (bAvg <= b)
+                {
+                    result += "1";
+                }
+                else
+                {
+                    result += "0";
+                }
+                ++bitCount;
+                if (bitCount == sizeof (int) * 8 && length < 0)
+                {
+                    //length = result.ToIntFromBinary();
+                    length = _size;
+                    result = "";
+                }
+
+                x += (delta + 1);
+                if (x >= image.Width - delta)
+                {
+                    y += (delta + 1);
+                    x = delta;
+                    if (y >= image.Height - delta)
+                        return AdjustLength(result).ToStringFromBinary();
+                }
+            };
+            return AdjustLength(result).ToStringFromBinary();
+        }
+
+
+
+        private string AdjustLength(string s)
+        {
+            var fixedText = s.TrimEnd(' ');
+            fixedText = fixedText.PadRight(8 * (int)Math.Ceiling((double)fixedText.Length / 8));
+            return fixedText;
+        }
+    }
+}
---
+++ KutterAlgorithm/KutterAlgorithm/ViewModel/MainViewModel.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Drawing;
+using System.IO;
+using GalaSoft.MvvmLight;
+using System;
+using GalaSoft.MvvmLight.Command;
+
+namespace KutterAlgorithm.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly KutterEncipherer _kutterEncipherer = new KutterEncipherer();
+        private string _originalText;
+        private string _imagePath;
+        private string _decryptedText;
+        private int _delta;
+        private double _alpha;
+
+
+        public string OriginalText
+        {
+            get
+            {
+                return _originalText;
+            }
+            set
+            {
+                if (_originalText == value) return;
+                _originalText = AdjustLength(value);
+                //Encrypt();
+                RaisePropertyChanged(() => OriginalText);
+                RaisePropertyChanged(() => OriginalTextAsBitArray);
+            }
+        }
+
+        public string ImagePath
+        {
+            get
+            {
+                return _imagePath;
+            }
+            set
+            {
+                if (_imagePath == value) return;
+                _imagePath = value;
+                RaisePropertyChanged(() => ImagePath);
+            }
+        }
+
+        public string DecryptedText
+        {
+            get
+            {
+                return _decryptedText;
+            }
+            set
+            {
+                if (_decryptedText == value) return;
+                _decryptedText = value;
+                RaisePropertyChanged(() => DecryptedText);
+                RaisePropertyChanged(() => DecryptedTextAsBitArray);
+            }
+        }
+
+        public string OriginalTextAsBitArray
+        {
+            get { return (_originalText ?? "").ToBitString(); }
+        }
+
+        //public string EncryptedTextAsBitArray
+        //{
+        //    get { return (_encryptedText ?? "").ToBitString(); }
+        //}
+
+        public string DecryptedTextAsBitArray
+        {
+            get { return (_decryptedText ?? "").ToBitString(); }
+        }
+
+        public int Delta
+        {
+            get
+            {
+                return _delta;
+            }
+            set
+            {
+                if (_delta == value) return;
+                _delta = value;
+                //Encrypt();
+                RaisePropertyChanged(() => Delta);
+            }
+        }
+
+        public double Alpha
+        {
+            get
+            {
+                return _alpha;
+            }
+            set
+            {
+                if (_alpha == value) return;
+                _alpha = value;
+                //Encrypt();
+                RaisePropertyChanged(() => Alpha);
+            }
+        }
+
+        public RelayCommand OpenImgCommand { get; private set; }
+        public RelayCommand EncodeCommand { get; private set; }
+
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            _delta = 2;
+            _alpha = 0.01;
+            _imagePath = @"C:\Users\Artyom\Desktop\TESV 2012-01-22 16-20-55-73.bmp";
+            OpenImgCommand = new RelayCommand(OpenImg);
+            EncodeCommand = new RelayCommand(Encode, () => !string.IsNullOrEmpty(ImagePath));
+            OriginalText =
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        }
+
+        private void OpenImg()
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog { DefaultExt = ".bmp", Filter = "Images|*.bmp;*.png" };
+            var result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                ImagePath = dlg.FileName;
+            }
+        }
+
+        private string AdjustLength(string s)
+        {
+            var fixedText = s.TrimEnd(' ');
+            fixedText = fixedText.PadRight(8 * (int)Math.Ceiling((double)fixedText.Length / 8));
+            return fixedText;
+        }
+
+        private void Encode()
+        {
+            try
+            {
+                var image = (Bitmap)Image.FromFile(ImagePath, true);
+                var newImg = _kutterEncipherer.Encode(OriginalText, image, Delta, Alpha);
+                var newPath = Path.Combine(Path.GetDirectoryName(ImagePath),
+                                           Path.GetFileNameWithoutExtension(ImagePath) + DateTime.Now.ToString("ddMMyyyyHHmmss") + Path.GetExtension(ImagePath));
+                newImg.Save(newPath);
+
+                newImg = (Bitmap)Image.FromFile(newPath, true);
+                try
+                {
+                    DecryptedText = _kutterEncipherer.Decode(newImg, Delta, Alpha);
+                }
+                catch (Exception)
+                {
+                    DecryptedText = "При декодировании возникли ошибки";
+                }
+            }
+            catch (Exception e)
+            {
+                System.Windows.MessageBox.Show(e.Message);
+            }
+        }
+
+        private void Decode()
+        {
+            //DecryptedText = _feistelEncipherer.Decrypt(EncryptedText, Delta);
+        }
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
